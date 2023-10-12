a55a534b24bc5c62f093c1a6935714c0b28d5e01
= <=>
2023-10-12 13:37:28 +0100

    task 1 - complete

---
+++ src/components/PuppyImage.tsx
@@ -0,0 +1,11 @@
+import Puppy from "../assets/puppy.png";
+
+function PuppyImage() {
+  return (
+    <>
+      <img alt="puppy" width="20%" src={Puppy} />
+    </>
+  );
+}
+
+export default PuppyImage;
